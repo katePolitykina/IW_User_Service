303584b07311700c5640033a483cf9a568efdbf4
ekaterinapolitykina <eka.politykina@gmail.com>
2025-07-18 14:04:30 -0400

    Keycloak stated

---
+++ src/main/java/org/example/userservice/security/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package org.example.userservice.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    @Value("${auth.jwks-uri}")
+    private String jwksUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/actuator").permitAll()
+                        .requestMatchers("/api/v1.0/users/**", "/api/v1.0/cards/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri(jwksUri)
+                        )
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtAuthenticationConverter());
+        return new JwtAuthenticationConverter();
+    }
+    }
+
